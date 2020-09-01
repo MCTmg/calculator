629bb0e74534c75bb275527e19c7570162f964b6
MCTmg <70583895+MCTmg@users.noreply.github.com>
2020-09-01 22:32:10 +0200

    komentarz
    demo 1234

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //oeratory + sa niezbedne
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
